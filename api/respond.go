c12d71a77b453d335c0afcbf6e387ee1c3ac8c09
liyu-wang <liyuwang1985@gmail.com>
2026-01-02 16:45:42 +1100

    api: Responding

---
+++ api/respond.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func decodeBody(r *http.Request, v any) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func encodeBody(w http.ResponseWriter, r *http.Request, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, status int, data any) {
+	w.WriteHeader(status)
+	if data != nil {
+		encodeBody(w, r, data)
+	}
+}
+
+func respondErr(w http.ResponseWriter, r *http.Request, status int, args ...any) {
+	respond(w, r, status, map[string]any{
+		"error": map[string]any{
+			"message": fmt.Sprint(args...),
+		},
+	})
+}
+
+func respondHTTPErr(w http.ResponseWriter, r *http.Request, status int) {
+	respondErr(w, r, status, http.StatusText(status))
+}
